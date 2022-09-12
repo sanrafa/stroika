934a5761817030d435321ec26a56ab04fc766482
Rafa Brown <shane.rafael@zohomail.com>
2022-09-11 21:48:33 -0600

    Add update task feature

--- src/lib/components/TaskView.tsx
+++ src/lib/components/TaskView.tsx
@@ -31,6 +31,9 @@ const Task = ({ id }: TaskProps) => {
   const task = useAppSelector((state) => state.tasks.entities[id]);
   const [completed, setCompleted] = React.useState(Boolean(task?.completed));
 
+  const [isEditing, setIsEditing] = React.useState(false);
+  const [desc, setDesc] = React.useState(task?.description);
+
   return (
     <li
       className={`flex space-x-2 px-4 py-1 rounded-md hover:bg-slate-700 ${
@@ -43,20 +46,45 @@ const Task = ({ id }: TaskProps) => {
           dispatch(toggleTaskComplete({ id, completed: !completed }));
           setCompleted(!completed);
         }}
-        className="bg-categoryToggleUnchecked w-5 h-5 flex justify-center items-center self-center shadow-inset rounded"
+        className="bg-categoryToggleUnchecked w-5 h-5 flex flex-shrink-0 justify-center items-center self-center shadow-inset rounded"
       >
         <CheckboxIndicator>
           <div className="bg-categoryToggleChecked w-5 h-5 shadow-[0px_2px_4px_rgba(0, 0, 0, 0.17)] rounded-sm"></div>
         </CheckboxIndicator>
       </Checkbox>
-      <span className="self-baseline text-center w-4/5">
-        {task?.description}
-      </span>
+
+      {isEditing ? (
+        <form
+          className="self-baseline"
+          onSubmit={(e) => {
+            e.preventDefault();
+            dispatch(updateTask({ id, changes: { description: desc } }));
+            setIsEditing(false);
+          }}
+        >
+          <input
+            tabIndex={1}
+            autoFocus
+            value={desc}
+            type="text"
+            className="text-center w-6/8 bg-slate-800 rounded mx-4"
+            onChange={(e) => setDesc(e.target.value)}
+          />
+          <button type="submit" className="hidden"></button>
+        </form>
+      ) : (
+        <span className="self-baseline text-center w-4/5">
+          {task?.description}
+        </span>
+      )}
+
       {!completed ? (
         <>
           <button
             type="button"
             className="text-feature rounded-full px-2 hover:text-categoryToggleUnchecked hover:bg-slate-600"
+            onClick={() => setIsEditing(true)}
+            disabled={isEditing}
           >
             <EditIcon />
           </button>
@@ -64,8 +92,9 @@ const Task = ({ id }: TaskProps) => {
             type="button"
             className="text-red-500 px-2 rounded-full hover:text-red-600 hover:bg-slate-600"
             onClick={() =>
-              dispatch(deleteTask({ id, featureId: task?.featureId }))
+              dispatch(deleteTask({ id, featureId: task?.featureId as string }))
             }
+            disabled={isEditing}
           >
             <DeleteIcon />
           </button>
